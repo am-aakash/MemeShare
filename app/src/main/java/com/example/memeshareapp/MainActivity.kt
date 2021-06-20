c269004528fb1b0931155ecdd4b9a8ac78dea706
am-aakash <akashchaudhary12121@gmail.com>
2021-06-20 23:38:50 +0530

    Logic added

--- app/src/main/java/com/example/memeshareapp/MainActivity.kt
+++ app/src/main/java/com/example/memeshareapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.memeshareapp
+import android.graphics.drawable.Drawable
 import kotlinx.android.synthetic.main.activity_main.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,6 +13,10 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +26,35 @@ class MainActivity : AppCompatActivity() {
         loadMeme()
     }
     private fun loadMeme() {
+        progressBar.visibility= View.VISIBLE
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener{ response ->
                 var url = response.getString("url")
-                Glide.with(this).load(url).into(meme)
+                Glide.with(this).load(url).listener(object: RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility= View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        progressBar.visibility=View.GONE
+                        return false
+                    }
+                }).into(meme)
             },
             {
                 Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
@@ -39,7 +66,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun shareMeme(view: View) {}
-    fun nextMeme(view: View) {}
+    fun nextMeme(view: View) {
+        loadMeme()
+    }
 
 
 }
